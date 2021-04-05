9b5f3d27af1a6398bf678f7f25f302baf14a520c
RickySandia <poedziee.ricky1528@gmail.com>
2021-04-04 21:05:16 -0700

    coding pertama

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>kalkulator sederhana</title>
+</head>
+<body>
+		<form action="proses.php"method="post">
+			<input type="text" name="angka1">
+			<input type="text" name="angka2">
+			<input type="submit" name="tambah"value="Tambah">
+		</form>
+	</body>
+</html>
---
+++ proses.php
@@ -0,0 +1,12 @@
+<?php 
+
+	$angka1 =  $_POST['angka1'];
+	$angka2 =  $_POST['angka2'];
+
+	$hasil = $angka1 + $angka2;
+
+ ?>
+
+
+ 	<h1> hasilnya adalah <?php echo $hasil ?></h1>
+ 	<a href="index.php"><h3>Hitung Lagi</h3></a>
