398104f121941799300515cf23ff16b7fa9f6669
RickySandia <zidanalfansha17@gmail.com>
2021-04-07 21:42:16 -0700

    kodepertama

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<form action="proses.php"method="post">
 			<input type="text" name="angka1">
 			<input type="text" name="angka2">
-			<input type="submit" name="tambah"value="Tambah">
+			<input type="submit" name="submit" value="Tambah">
 		</form>
 	</body>
 </html>
